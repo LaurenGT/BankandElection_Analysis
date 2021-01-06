a3db8af64b2e139b929653b47733f3e9b2225a3a
LaurenGT <ltoothaker2@gmail.com>
2021-01-06 14:57:23 -0800

    new version of code collecting all values in appended lists. Missing dates associated with min/max

--- PyBank/main.py
+++ PyBank/main.py
@@ -21,12 +21,15 @@ pyBank_csv = os.path.join(
 #     pnl = int(bank_data[1])
 
 # set total_months count to 0 for start of loops below
-total_months = 0
+# total_months = 0
 # set total_pnl count to 0 for start of loops below
-total_pnl = 0
+# total_pnl = 0
 
-# capture all pnl changes
+# capture all pnl changes in a dictionary and months in dictionary
 all_pnl_changes = []
+total_months = []
+net_pnl = []
+
 # open csv, read csv with delimiter as comma and print the header row
 with open(pyBank_csv, newline='') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
@@ -41,28 +44,44 @@ with open(pyBank_csv, newline='') as csv_file:
 
     # calculate total months in data set by looping through rows and adding 1 each time
     for row in csv_reader:
-        total_months += 1
-        print(total_months)
+        total_months.append(row[0])
+        # total_months += 1
+        # print(total_months)
 
         # calculate total profit and losses (total_pnl) of data set
-        total_pnl = (total_pnl + int(row[1]))
-        print(total_pnl)
+        # net_pnl = (net_pnl + int(row[1]))
+        # print(total_pnl)
+        net_pnl.append(int(row[1]))
 
         # calculate changes in pnl over data set and find average
         # find difference in pnl value between each row
         pnl_change = (int(row[1]) - previous_row)
-        print(pnl_change)
+        # print(pnl_change)
         # append all_pnl_changes list to use pull average later
         all_pnl_changes.append(pnl_change)
-        print(all_pnl_changes)
+        # print(all_pnl_changes)
 
         # is there a better way of doing this, or will this suffice for the purpose of the homework, setting the intitital previous_row to equal the value of the first month's pnl?
-
-        # calculate average pnl_change
 
         # sets new previous row reference value for calculation of pnl_change
         previous_row = int(row[1])
     
-    #calculate ave_pnl_changes over data set
-    ave_pnl_changes = int(all_pnl_changes) / len(int(total_months))
-    print(ave_pnl_changes)
+        #calculate ave_pnl_changes over data set
+        # ave_pnl_changes = sum(all_pnl_changes) / len(pnl_change)
+        # print(ave_pnl_changes)
+
+        # Greatest increase in profits (date and amount) over the entire period
+        # greatest_increase = max(all_pnl_changes)
+        # print(greatest_increase)
+            
+        # greatest_decrease = min(all_pnl_changes)
+        # print(greatest_decrease)
+
+    print(f"Total Months: {len(total_months)}")
+    print(f"Total Profits and Losses: {sum(net_pnl)}")
+    print(f"Greatest Monthly Increase: {max(all_pnl_changes)}")
+    print(f"Greatest Monthly Decrease: {min(all_pnl_changes)}")
+    ## print(all_pnl_changes)
+    print(f"Average Monthly Change: {sum(all_pnl_changes)/len(all_pnl_changes)}")
+
+    
