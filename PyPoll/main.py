584bbd6b7cc53f9106411ef91dd17dc920a04d41
LaurenGT <ltoothaker2@gmail.com>
2021-01-07 11:05:48 -0800

    collecting each voter ID in a list to count and print total votes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,6 +7,8 @@
 import os
 import csv
 
+total_votes = []
+
 # set file path for csv
 pyPoll_csv = os.path.join("resources","02-Homework_03-Python_PyPoll_Resources_election_data.csv")
 
@@ -16,3 +18,12 @@ with open(pyPoll_csv, newline = '') as csv_file:
     #skip header row
     csv_header = next(csv_file)
     print(csv_header)
+
+    for row in csv_reader:
+        #append total_votes list
+        total_votes.append(row[0])
+        
+    print(len(total_votes))
+    
+
+
