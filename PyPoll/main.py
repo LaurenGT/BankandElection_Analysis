f348dab3fdf47265f1f63e3387126bc5fcf67ad7
LaurenGT <ltoothaker2@gmail.com>
2021-01-07 17:04:42 -0800

    cleaned up comments and formatting. Need to export final data to text file.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,8 +1,3 @@
-# calculate total number of votes cast
-# Create a list of unique candidates who received votes
-# Calculate percentage of votes each candidate received of the total votes cast
-# Calculate the total number of votes each candidate received of the total votes cast
-# Who won the election based on popular vote? Whoe has the highest number or votes?
 
 import os
 import csv
@@ -10,9 +5,11 @@ import csv
 #set total_votes tracker to 0 for start of loop
 total_votes=0
 
+# list to store unique candidate names as iterating through loops
 unique_candidates = []
+# dictionary to store vote tallies per candidate as iterating through loops
 candidate_votes = {}
-# printed as 'Name' : vote count
+
 # set file path for csv
 pyPoll_csv = os.path.join("resources","02-Homework_03-Python_PyPoll_Resources_election_data.csv")
 
@@ -21,8 +18,8 @@ with open(pyPoll_csv, newline = '') as election_data:
 
     #skip header row
     csv_header = next(election_data)
-    print(csv_header)
-
+    
+    # start looping through rows to collect data
     for row in csv_reader:
 
         # assign column index names for readibility
@@ -39,23 +36,25 @@ with open(pyPoll_csv, newline = '') as election_data:
             candidate_votes[candidate_name] = 0
         
         candidate_votes[candidate_name] += 1
-    
+
+    # print election results available from this iterative loop
+    print("Election Results")
+    print("--------------------")
+    print(F"Total Votes: {total_votes}")
+    print("--------------------")
+    print(f"Vote Tally:")
+
     # pull the candidate votes out of the cadidate dictionary and calculate the percentage of the total votes
     for candidate in candidate_votes:
         percentage_votes = candidate_votes.get(candidate)
         print(f"{candidate}: {round((percentage_votes/total_votes)*100,2)}% ({percentage_votes})")
 
+    # print remaining results
+    print("--------------------")
     winning_votes = max(candidate_votes, key=candidate_votes.get)
     print(f"Winner: {winning_votes}")
+    print("---------------------")
 
-        # voting_numbers = candidate_votes.values()
-        # winning_votes = max(voting_numbers)
-        # print(f"{candidate} winning_votes")
-
-    print(total_votes)
-    print(unique_candidates)
-    print(candidate_votes)
-    # print(percentage_votes)
     
     
 
